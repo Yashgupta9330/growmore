599d7bcca4343d307b1041a13e89ac4c3063a7f0
YASH GUPTA <yaahg342@gmail.com>
2024-07-05 03:34:01 +0530

    Update login.tsx

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,6 +2,19 @@ import UserForm from "../components/UserForm";
 
 
 const Login : React.FC= () => {
+     const userData = localStorage.getItem("userData");
+     const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkCredentials = () => {
+      if (userData) {
+        navigate("/post");
+      }
+    };
+
+    checkCredentials();
+  }, [userData, navigate]);
+    
     return(
       <div>
       <UserForm/>
